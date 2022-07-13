644948a129d9e5796e5e7b84dc240ac617ba2c9e
Patricio Vargas <pato@onesignal.com>
2022-07-13 16:20:37 +0200

    added custom code

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //Your OneSignal Code Here
 window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function() {
+OneSignal.push(function() {
     OneSignal.init({
-        appId: "YOUR-ONESIGNAL-APP-ID",
+        appId: "fc80dd97-018e-4f0f-960e-7fed9b9a4df2",
+        safari_web_id: "web.onesignal.auto.5ccade99-0f35-4775-9ae0-5e2c3bfd110b",
+        notifyButton: {
+            enable: true,
+            size: "small",
+            position: "bottom-left",
+        },
+        allowLocalhostAsSecureOrigin: true,
     });
 });
